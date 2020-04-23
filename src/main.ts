cfd9e8528706999d46ddc0ba22614d79a094fbed
Bryant Biggs <bryantbiggs@gmail.com>
2020-04-23 12:55:54 -0400

    feat: add input values for commands options and file

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,24 @@
 import * as core from '@actions/core'
-// import stringArgv from 'string-argv'
+import stringArgv from 'string-argv'
 
 import Upx from './upx'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    const commands = stringArgv(core.getInput('commands', {required: false}).trim())
+    const options = stringArgv(core.getInput('options', {required: false}).trim())
+    const file = stringArgv(core.getInput('file', {required: true}).trim())
 
     const upx = await Upx.getOrInstall()
-    const result = await upx.callStdout(['--version'])
-    core.debug(result)
+    // upx [-123456789dlthVL] [-qvfk] [-o file] file..
+    // upx [commands] [options] file
+    const args = [...commands, ...options, file]
+
+    const version = await upx.callStdout(['--version'])
+    core.debug(version)
+
+    const result = await upx.callStdout(args)
+    core.setOutput('output', result)
   } catch (error) {
     core.setFailed(error.message)
   }
